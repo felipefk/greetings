515bc407cb07955dd8bbfe2ef9ff744c104ee9bd
felipefk <felipefk>
2024-01-11 21:15:17 +0100

    Add request id to trace and debug customer requests

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from middleware import request_id_middleware
 from routers import greeting
 
 app = FastAPI()
 
-# Include router from greeting.py
+# Add request ID middleware
+app.middleware("http")(request_id_middleware.request_id_middleware)
+
+# Include routers
 app.include_router(greeting.router)
